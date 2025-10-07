d57e446b78e118d786332a0e5796178a736e2bd6
Luka <lukasalipur.dev@gmail.com>
2025-10-07 15:17:17 +0200

    Initial Commit

---
+++ RickAndMorty2025/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RickAndMorty2025
+//
+//  Created by Luka Šalipur on 7. 10. 2025..
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
